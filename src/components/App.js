4b22452850f6e1489bef162acd55d180e6f150da
Marcielo Bell <marcielobell@gmail.com>
2020-03-01 22:15:22 -0800

    Added Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import Header from "./Header";
 
 const Button = styled.button`
-    cursor: pointer;
-    background: transparent;
-    font-size: 16px;
-    border-radius: 3px;
-    color: ${props => (props.primary ? 'palegreen' : 'palevioletred')}};
-    border: ${props =>
-    props.primary ? '2px solid palegreen' : '2px solid palevioletred'};
-    margin: 0 1em;
-    padding: 0.25em 1em;
-    transition: 0.5em all ease-out;
+  cursor: pointer;
+  background: transparent;
+  font-size: 16px;
+  border-radius: 3px;
+  color: ${props => (props.primary ? "palegreen" : "palevioletred")}};
+  border: ${props =>
+    props.primary ? "2px solid palegreen" : "2px solid palevioletred"};
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.5em all ease-out;
 
-    &:hover {
-        color: white;
-        background-color: ${props =>
-        props.primary ? 'palegreen' : 'palevioletred'};
-    }
+  &:hover {
+    color: white;
+    background-color: ${props =>
+      props.primary ? "palegreen" : "palevioletred"};
+  }
 `;
 
 function App() {
-    return (
-        <div>
-            <h1>Hello IG Clone</h1>
-            <Button> Styled Button Component</Button>
-            <Button primary> Primary Btn </Button>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <h1>Hello IG Clone</h1>
+      <Button> Styled Button Component</Button>
+      <Button primary> Primary Btn </Button>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
