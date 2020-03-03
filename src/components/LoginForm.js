6b1efa79aca9a3f910d61196c14ea399fca536fe
Marcielo Bell <marcielobell@gmail.com>
2020-03-03 12:55:37 -0800

    Styled components added and LoginForm work in progress

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components"
+
+
+const LoginFormWrapper = styled.form`
+  margin: auto;
+`;
+
+// const SignInEmailInput = style
+
+
+
+
+export default LoginForm
